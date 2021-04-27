359cd74093711b15b5588a565df83fbfb812fd92
Dmitriy Mironenko <dim@uain.su>
2021-04-27 17:05:32 +0300

    updated

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export const CreateScreen = ({}) => {
+
+  return (
+    <View style={styles.center}>
+      <Text>CreateScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export const MainScreen = ({}) => {
+
+  return (
+    <View style={styles.center}>
+      <Text>MainScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
